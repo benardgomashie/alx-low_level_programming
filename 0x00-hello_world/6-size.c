7162ca2c4a45258692bb6e9c6de4b3f0f93cc358
benardgomashie <benardgomashie@gmail.com>
2022-07-01 13:46:24 -0700

    6-size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main funt
+ * Return: int 0
+ */
+int main(void)
+{
+	char charType;
+	int intType;
+	long int longIntType;
+	long long int longLongType;
+	float floatType;
+
+	printf("Size of a char: %1d byte(s)\n", sizeof(chartype));
+	printf("Size of an int: %1d byte(s)\n", sizeof(IntType));
+	printf("Size of a long int: %1d byte(s)\n", sizeof(longIntType));
+	printf("Size of a long Long int: %1d byte(s)\n", sizeof(LongLongIntType));
+	printf("size of a float: %1d byte(s)\n", sizeof(floatType));
+	return (0);
+}
