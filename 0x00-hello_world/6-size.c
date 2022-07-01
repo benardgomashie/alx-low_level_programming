d0fd6abf85063a356f995e8acd4d1fe0b56a97e9
benardgomashie <benardgomashie@gmail.com>
2022-07-01 14:09:47 -0700

    git reserve

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,16 +5,16 @@
  */
 int main(void)
 {
-	char charType;
-	int intType;
-	long int longIntType;
-	long long int longLongType;
-	float floatType;
+	char cT;
+	int iT;
+	long int lIT;
+	long long int lLIT;
+	float fT;
 
-	printf("Size of a char: %1d byte(s)\n", sizeof(chartype));
-	printf("Size of an int: %1d byte(s)\n", sizeof(IntType));
-	printf("Size of a long int: %1d byte(s)\n", sizeof(longIntType));
-	printf("Size of a long Long int: %1d byte(s)\n", sizeof(LongLongIntType));
-	printf("size of a float: %1d byte(s)\n", sizeof(floatType));
+	printf("Size of a char: %1d byte(s)\n", sizeof(cT));
+	printf("Size of an int: %1d byte(s)\n", sizeof(iT));
+	printf("Size of a long int: %1d byte(s)\n", sizeof(lIT));
+	printf("Size of a long Long int: %1d byte(s)\n", sizeof(lLIT));
+	printf("size of a float: %1d byte(s)\n", sizeof(fT));
 	return (0);
 }
