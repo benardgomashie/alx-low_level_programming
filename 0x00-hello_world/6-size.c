31cd1a602d44db3b918ab20dbf7c82b99b6d3c1d
benardgomashie <benardgomashie@gmail.com>
2022-07-02 08:35:03 -0700

    bit change

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int main(void)
 	long long int lLIT;
 	float fT;
 
-	printf("Size of a char: %1d byte(s)\n", sizeof(cT));
-	printf("Size of an int: %1d byte(s)\n", sizeof(iT));
-	printf("Size of a long int: %1d byte(s)\n", sizeof(lIT));
-	printf("Size of a long Long int: %1d byte(s)\n", sizeof(lLIT));
-	printf("size of a float: %1d byte(s)\n", sizeof(fT));
+	printf("Size of a char: %zu byte(s)\n", sizeof(cT));
+	printf("Size of an int: %zu byte(s)\n", sizeof(iT));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(lIT));
+	printf("Size of a long Long int: %zu byte(s)\n", sizeof(lLIT));
+	printf("size of a float: %zu byte(s)\n", sizeof(fT));
 	return (0);
 }
