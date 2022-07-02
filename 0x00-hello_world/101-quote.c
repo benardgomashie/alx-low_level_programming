64b0cfab0b12336485b95ba5958cfbb5f4a09ce1
benardgomashie <benardgomashie@gmail.com>
2022-07-02 09:41:25 -0700

    error direct

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - print a qoute
+ * Return: an integer,1
+ */
+int main(void)
+{
+	printf(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+	return(1);
+}
