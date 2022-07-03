cf3d64cbb97316f671ee8bb22614586ba2fbf29d
benardgomashie <benardgomashie@gmail.com>
2022-07-02 20:21:46 -0700

    c file 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,7 @@
  */
 int main(void)
 {
-	printf(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
-	return(1);
+	char var[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, var, sizeof(var) - 1);
+	return (1);
 }
