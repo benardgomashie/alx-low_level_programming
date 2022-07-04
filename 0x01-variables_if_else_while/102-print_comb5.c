4b76ff34a0fab2121ee625431881e169333f0253
benardgomashie <benardgomashie@gmail.com>
2022-07-04 15:09:54 -0700

    low level final

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - output all single digits
+ * Return: 0 is green
+ */
+int main(void)
+{
+	int var;
+
+	for (var = 0; var < 99; var++)
+	{
+		int ii;
+
+		for (ii = var + 1; ii < 100; ii++)
+		{
+			putchar((var / 10) + '0');
+			putchar((var % 10) + '0');
+			putchar(' ');
+			putchar((ii / 10) + '0');
+			putchar((ii % 10) + '0');
+
+			if (var != 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			else
+				continue;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
