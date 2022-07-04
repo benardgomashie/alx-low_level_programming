2452496b377fc09171cd77fa27947cc546af1e09
benardgomashie <benardgomashie@gmail.com>
2022-07-04 11:56:11 -0700

    alphabetos

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - alphabetical
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	char var;
+	for (var = 'a'; var <= 'z'; var++)
+	{
+		putchar(var);
+	}
+	putchar('\n');
+	return (0);
+}
