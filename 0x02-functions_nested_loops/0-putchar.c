edf24bd749c71f98d4f62134d48d231fb3877377
benardgomashie <benardgomashie@gmail.com>
2022-07-05 17:40:31 -0700

    puchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include <main.h>
 /**
  * main - this main function is a printer
  * Return: should return 0
  */
 int main(void)
 {
-	printf("_putchar");
+	printf("_putchar\n");
 	return (0);
 }
