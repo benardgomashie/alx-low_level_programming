0bd15b4f3a1d82c57747e0b726161f54716441c2
benardgomashie <benardgomashie@gmail.com>
2022-07-05 18:55:32 -0700

    tough

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,20 @@
-#include <main.h>
+#include "main.h"
 /**
  * main - this main function is a printer
  * Return: should return 0
  */
 int main(void)
 {
-	printf("_putchar\n");
+	char p[] = "_putchar";
+	int r;
+
+	int lenVar = sizeof(p) / sizeof(p[0]);
+
+	for (r = 0; r < lenVar - 1; r++)
+	{
+		_putchar(p[r]);
+	}
+		_putchar('\n');
+
 	return (0);
 }
