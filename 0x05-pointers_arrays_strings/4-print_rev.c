63ffff312b2ac802c4dd615a45e3854b2f0e98ce
benardgomashie <benardgomashie@gmail.com>
2022-07-11 16:10:11 -0700

    first reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - prints str in reverse
+ * @s: str
+ * return: 0
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+		while (s[i])
+			i++;
+		while (1--)
+			printf("s[i]\n");
+}
