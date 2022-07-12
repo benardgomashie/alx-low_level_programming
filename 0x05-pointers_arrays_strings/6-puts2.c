53fc165fb752b9418d74cc9b39137987b7c9522a
benardgomashie <benardgomashie@gmail.com>
2022-07-12 15:09:38 -0700

    first push for 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other charater of a string with first charater
+ * @str: string to be manipulated
+ * Return: returns as void
+ */
+
+void puts2(char *str)
+{
+	char *ptr = str;
+	int len = 0;
+	int;
+
+	while (*ptr != '\0')
+	{
+		len++;
+		ptr++;
+	}
+
+	for (j = 0; j <= (len -1); j++)
+	{
+		if (j % 2 == 0)
+			_putchar(str[j]);
+	}
+	_putchar('\n')
+}
