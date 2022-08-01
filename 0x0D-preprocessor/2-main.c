3f6ca210a9e9a71126f2fdc09b5f89b74eded82e
benardgomashie <benardgomashie@gmail.com>
2022-08-01 11:46:21 +0100

    main

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * main - main function for printing file name
+ * Return: 0
+ * 
+ */
+int main (void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
